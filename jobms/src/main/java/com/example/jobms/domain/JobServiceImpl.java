9192617f007c0fd9ed18ed5a0014002967bd989d
owner <toltolyibh@gmail.com>
2024-09-18 11:17:27 +0900

    rate limiter 구현

--- jobms/src/main/java/com/example/jobms/domain/JobServiceImpl.java
+++ jobms/src/main/java/com/example/jobms/domain/JobServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.jobms.external.Review;
 import com.example.jobms.infrastructure.JobRepository;
 import com.example.jobms.mapper.JobMapper;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -33,7 +34,8 @@ public class JobServiceImpl implements JobService {
 
     @Override
 //    @CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
-    @Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+//    @Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
     public List<JobDTO> findAll() {
         return jobRepository.findAll().stream()
                 .map(this::convertToDto)
