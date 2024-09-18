79fa68e6b0a7e424dedffce6dbfc69dbb39a4973
owner <toltolyibh@gmail.com>
2024-09-18 13:03:11 +0900

    Rabbitmq 설정

---
+++ companyms/src/main/java/com/example/companyms/messaging/ReviewMessageConsumer.java
@@ -0,0 +1,20 @@
+package com.example.companyms.messaging;
+
+import com.example.companyms.domain.CompanyService;
+import com.example.companyms.dto.ReviewMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewMessageConsumer {
+    private final CompanyService companyService;
+
+    public ReviewMessageConsumer(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @RabbitListener(queues = "companyRatingQueue")
+    public void consumeMessage(ReviewMessage reviewMessage) {
+        companyService.updateCompanyRating(reviewMessage);
+    }
+}
